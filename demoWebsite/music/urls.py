62963d480e9251dcd8261d3c154a5436c277a08a
muin2473 <muin.739@gmail.com>
2020-08-25 13:31:06 +0600

    added "Detailed view"

--- demoWebsite/music/urls.py
+++ demoWebsite/music/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /music/
     path('', views.index, name='index'),
+
+    # /music/album_id/
+    path('<int:album_id>/', views.detail, name='detail'),
 ]
